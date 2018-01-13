504a759300725c70847589727e50026d0bc0eada
seedgabo <seedgabo@gmail.com>
2018-01-13 08:31:12 -0500

    home

--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -26,12 +26,13 @@ export class ResultsPage {
   ionViewDidLoad() {
     var ctx = document.getElementById("myChart");
     var myChart = new Chart(ctx, {
-      type: 'bar',
+      type: 'pie',
       data: {
         datasets: [{
           data: [this.results.result1, this.results.result2, this.results.result3],
           label: "# de Votos",
-          backgroundColor: ["Red", "Green", "Blue"],
+          backgroundColor: ['rgba(255, 99, 132, 0.8)', 'rgba(54, 162, 235, 0.8)', 'rgba(255, 206, 86, 0.8)', 'rgba(75, 192, 192, 0.8)', 'rgba(153, 102, 255, 0.8)', 'rgba(255, 159, 64, 0.8)'],
+          borderColor: ['rgba(255,99,132,1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
         }],
 
         // These labels appear in the legend and in the tooltips when hovering different arcs
